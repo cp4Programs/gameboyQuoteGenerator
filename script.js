a892f36631ea6dee045572d7e83fa6dd453184ba
Carlo Perito <carlop@Carlos-MacBook-Pro.local>
2022-10-10 23:49:54 -0700

    random quote and previous quote

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
-const quoteButton = document.querySelector("#quote-screen");
+const quoteButton = document.querySelector(".arrow-button1");
 const content = document.querySelector("#content");
 const author = document.querySelector("#author");
+const display = document.querySelector(".grey-button1")
+const quoteList = [];
+const authorList = [];
 
-
-quoteButton.addEventListener(".arrow-button1", () => {
+quoteButton.addEventListener("click", () => {
 
     const index = Math.floor(Math.random() * 10)
     const page = Math.floor(Math.random() * 7268)
-    fetch(`https://quote-garden.herokuapp.com/api/v3/quotes${page}`)
-        .then(response => response.Json())
-        .then(response => {
+    fetch(`https://quote-garden.herokuapp.com/api/v3/quotes?=page${page}`)
+        .then(res => res.json())
+        .then(res => {
             console.log(res.data[index].quoteText)
             console.log(res.data[index].quoteAuthor)
-        })
 
-    console.log(index);
-    console.log(page);
+            content.innerText = `"${res.data[index].quoteText}"`;
+            author.innerText = res.data[index].quoteAuthor;
+            quoteList.push(res.data[index].quoteText)
+            authorList.push(res.data[index].quoteAuthor)
+        })
 })
 
+const previousQuote = document.querySelector(".arrow-button2");
+previousQuote.addEventListener("click", () => {
+    content.innerText = `"${quoteList[quoteList.length - 2]}"`;
+    author.innerText = authorList[authorList.length - 2];
+})
 
+display.addEventListener("click", () => {
+    console.log(quoteList, authorList);
+})
